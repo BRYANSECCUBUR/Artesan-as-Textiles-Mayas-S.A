0a085790e36ff747479f29b01bb796ad9314a202
bcoci-umg <bcoci@miumg.edu.gt>
2026-08-30 04:55:21 +0000

    Agregar avance completo del proyecto

---
+++ frontend/public/js/producto.js
@@ -0,0 +1,35 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Traje de Patzún",
+        municipio: "Patzún",
+        departamento: "Chimaltenango",
+        categoria: "Traje típico",
+        color: "Rojo",
+        precio: 350,
+        imagen: "./img/patzun.png",
+        descripcion:"traje de patzun"
+    },
+    {
+        id: 2,
+        nombre: "Huipil de Santiago Atitlán",
+        municipio: "Santiago Atitlán",
+        departamento: "Sololá",
+        categoria: "Huipil",
+        color: "Multicolor",
+        precio: 450,
+        imagen: "./img/images.png",
+        descripcion:"huipil de santiago atitlan"
+    },
+    {
+        id: 3,
+        nombre: "Corte tradicional",
+        municipio: "San Juan Sacatepéquez",
+        departamento: "Guatemala",
+        categoria: "Corte",
+        color: "Azul",
+        precio: 300,
+        imagen: "./img/images.png",
+        descripcion:"corte tradicional"
+    }
+];
